1f1616ce1e61102f00d610048cc9e21fde685f64
dytsou <d.y.tsou@gmail.com>
2023-08-19 00:51:35 +0800

    modify it into linebot ver. and add sticker msg

--- botapp.py
+++ botapp.py
@@ -1,26 +1,22 @@
 from flask import Flask, request, abort
 
-from linebot.v3 import (
+from linebot import (
+    LineBotApi,
     WebhookHandler
 )
-from linebot.v3.exceptions import (
+from linebot.exceptions import (
     InvalidSignatureError
 )
-from linebot.v3.messaging import (
-    Configuration,
-    ApiClient,
-    MessagingApi,
-    ReplyMessageRequest,
-    TextMessage
-)
-from linebot.v3.webhooks import (
+from linebot.models import (
     MessageEvent,
-    TextMessageContent
+    TextMessage,
+    TextSendMessage,
+    StickerSendMessage
 )
 
 app = Flask(__name__)
 
-configuration = Configuration(access_token='fPzcWsxdrtZiGB9HJ2JtTAcDbxgeGZWp+dxmrh7VgFTzUItPE6MjDjaP8K7uRM0KZRIJN+kAZyXFrBoFwc2gHP6MogNU3c5PMJjmjcH5NMbHw92ZLvPu5BfU2oWHx9b2pJDfV4/ZuyPxH/0Q61UcbgdB04t89/1O/w1cDnyilFU=')
+line_bot_api = LineBotApi('fPzcWsxdrtZiGB9HJ2JtTAcDbxgeGZWp+dxmrh7VgFTzUItPE6MjDjaP8K7uRM0KZRIJN+kAZyXFrBoFwc2gHP6MogNU3c5PMJjmjcH5NMbHw92ZLvPu5BfU2oWHx9b2pJDfV4/ZuyPxH/0Q61UcbgdB04t89/1O/w1cDnyilFU=')
 handler = WebhookHandler('d4c3581384cbbe2b90f6298ec3cac353')
 
 
@@ -43,32 +39,43 @@ def callback():
     return 'OK'
 
 
-@handler.add(MessageEvent, message=TextMessageContent)
+@handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
-    with ApiClient(configuration) as api_client:
-        line_bot_api = MessagingApi(api_client)
-        message_text = str(event.message.text).lower()
-        if message_text == '@about':
-            emoji = [
-                {
-                    "index": 0,
-                    "productId": "5ac1bfd5040ab15980c9b435",
-                    "emojiId": "001"
-                },
-                {
-                    "index": 13,
-                    "productId": "5ac1bfd5040ab15980c9b435",
-                    "emojiId": "002"
-                },
-            ]
-            text_message = TextMessage(text="$歡迎使用linebot $",emojis=emoji)
-        else:
-            text_message = TextMessage(text=event.message.text)
-        line_bot_api.reply_message_with_http_info(
-            ReplyMessageRequest(
-                reply_token=event.reply_token,
-                messages=[text_message]
-            )
+    message_text = str(event.message.text).lower()
+    if message_text == '@about': # 關鍵字
+        emoji = [ # 貼圖
+            {
+                "index": 0, # 第幾個字元
+                "productId": "5ac1bfd5040ab15980c9b435", # 貼圖包ID
+                "emojiId": "001" # 貼圖ID
+            },
+            {
+                "index": 12, # 第幾個字元
+                "productId": "5ac1bfd5040ab15980c9b435", # 貼圖包ID
+                "emojiId": "002" # 貼圖ID
+            },
+        ]
+        text_message = TextMessage(text="$歡迎使用linebot$", emojis=emoji) # 回覆文字
+        sticker_message = StickerSendMessage( # 回覆貼圖
+            package_id='446', # 貼圖包ID
+            sticker_id='1988' # 貼圖ID
+        )
+        line_bot_api.reply_message( # 回覆訊息
+            event.reply_token,
+            [text_message, sticker_message]
+        )
+    else:
+        emoji = [
+            {
+                "index": 0,
+                "productId": "5ac21a18040ab15980c9b43e",
+                "emojiId": "014"
+            }, 
+        ]
+        text_message = TextMessage(text="$" + event.message.text, emojis=emoji)
+        line_bot_api.reply_message( 
+            event.reply_token,
+            text_message
         )
 
 if __name__ == "__main__":
