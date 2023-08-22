412fe726b8e2c165a23d9d5abd1351dd07d1b1cf
dytsou <d.y.tsou@gmail.com>
2023-08-23 00:22:22 +0800

    sql import

--- app.py
+++ app.py
@@ -55,6 +55,18 @@ def callback():
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
     message_text = str(event.message.text).lower()
+    user = User.query.filter(User.line_id == event.source.user_id).first() # 查詢使用者是否存在
+    # print(event.source.user_id) 
+    if not user:
+        profile = line_bot_api.get_profile(event.source.user_id) # 取得使用者資料
+        # print(profile.display_name)
+        # print(profile.user_id)
+        # print(profile.picture_url)
+        # print(profile.status_message)
+        new_user = User(profile.user_id, profile.display_name, profile.picture_url) # 建立新使用者
+        db.session.add(new_user) # 新增使用者到資料庫
+        db.session.commit() # 儲存
+    
     if message_text == '@about': # 關鍵字
         emoji = [ # 貼圖
             {
